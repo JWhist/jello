70d27153bf53a19a75c5418f052dea801e06aebb
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-10-05 08:15:43 -0700

    add get board route
    Co-authored-by: Jordan <whistlerjordan@gmail.com>

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,4 +7,6 @@ router.get("/boards", boardsController.getBoards);
 
 router.post("/boards", validateBoard, boardsController.createBoard);
 
+router.get("/boards/:id", boardsController.getBoardById);
+
 module.exports = router;
